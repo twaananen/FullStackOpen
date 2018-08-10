33b66dbe814aec2fc9110fd2dd0558d9a0c0f362
Tommi Wäänänen <tommiwaananen@gmail.com>
2018-08-10 18:50:35 +0300

    submission for exercise 1.6

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,65 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Otsikko = (props) => {
-  return (
-    <div>
-      <h1>{props.kurssi} </h1>
-    </div>
-  )
-}
 
-const Osa = (props) => {
-  return (
-    <div>
-      <p>{props.osa.nimi} {props.osa.tehtavia}</p>
-    </div>
-  )
-}
 
-const Sisalto = (props) => {
-  return (
-    <div>
-      < Osa osa = {props.osat[0]} />
-      < Osa osa = {props.osat[1]} />
-      < Osa osa = {props.osat[2]} />
-    </div>
-  )
-}
-
-const Yhteensa = (props) => {
-  return (
-    <div>
-      <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
+class App extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state ={
+      hyvä: 0,
+      neutraali: 0,
+      huono: 0
+    }
   }
-  return (
-    <div>
-      <Otsikko kurssi={kurssi.nimi} />
-      <Sisalto osat ={kurssi.osat} />
-      <Yhteensa osat ={kurssi.osat}/>
-    </div>
-  )
+  
+  render(){
+    return <div>
+      <p><strong>anna palautetta</strong></p>
+      <button onClick={() => {this.setState({hyvä: this.state.hyvä+1})}}>hyvä</button>
+      <button onClick={() => {this.setState({neutraali: this.state.neutraali+1})}}>neutraali</button>
+      <button onClick={() => {this.setState({huono: this.state.huono+1})}}>huono</button>
+      <p><strong>statistiikka</strong></p>
+      hyvä {this.state.hyvä}<br></br>
+      neutraali {this.state.neutraali}<br></br>
+      huono {this.state.huono}
+      </div>
+  }
 }
 
 ReactDOM.render(
