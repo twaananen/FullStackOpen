53aeaeef62d30ee0f321601e81b22d8f5af0fd72
Tommi Wäänänen <tommiwaananen@gmail.com>
2018-08-11 21:16:23 +0300

    submission for exercise 2.6

--- 1/src/App.js
+++ 1/src/App.js
@@ -1,15 +1,49 @@
-import React from 'react'
-import Kurssi from "./components/Kurssi";
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
 
+  handleInputChange = (event) => {
+    this.setState({newName: event.target.value})
+  }
 
-const App = ({kurssit}) => {
+  addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: this.state.newName
+    }
+    const persons = this.state.persons.concat(newPerson)
+    this.setState({
+      persons,
+      newName: ""
+    })
+  }
 
-  return (
-    <div>
-      {kurssit.map(kurssi => <Kurssi kurssi={kurssi}></Kurssi>)}
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleInputChange}/>
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map(person =><div key={person.name}>{person.name}</div>)}
+      </div>
+    )
+  }
 }
 
 export default App
