d3587ab21e8a807b7496c919660d51e40d470bee
Tommi Wäänänen <tommiwaananen@gmail.com>
2018-08-18 00:57:19 +0300

    course tutorial part 2 final

--- Tutorials/osa2/src/App.js
+++ Tutorials/osa2/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Note from './components/Note'
-import axios from "axios";
 import noteService from "./services/notes"
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +19,8 @@ class App extends React.Component {
     this.state = {
       notes: [],
       newNote: 'uusi muistiinpano..',
-      showAll: true
+      showAll: true,
+      error: null
     }
     console.log("constructor");
   }
@@ -64,9 +75,13 @@ class App extends React.Component {
           })
         })
         .catch(error => {
-          alert(`muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`)
-          this.setState({ notes: this.state.notes.filter(n => n.id !== id) })
+          this.setState({ 
+            error: `muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`,
+            notes: this.state.notes.filter(n => n.id !== id) })
         })
+        setTimeout(() => {
+          this.setState({error:null})
+        }, 5000);
     }
   }
   
@@ -82,6 +97,7 @@ class App extends React.Component {
     return (
       <div>
       <h1>Muistiinpanot</h1>
+      <Notification message = {this.state.error} />
       <div>
       <button onClick={this.toggleVisible}>
       näytä {label}
--- Tutorials/osa2/src/index.js
+++ Tutorials/osa2/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App";
+import "./index.css"
 
 //C:\Users\Tommi\Google Drive\Opiskelu\FullStackWebDev\Harjoitukset\osa1
 //C:\Users\Tommi\Google Drive\Opiskelu\FullStackWebDev\Harjoitukset\repo\FullStackOpen\1
